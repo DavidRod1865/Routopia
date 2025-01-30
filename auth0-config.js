d6082d9b00c2d7a02cc1a9e1961ab7c28e4194e7
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2025-01-29 23:24:34 -0500

    Updated Auth0 Redirect

--- auth0-config.js
+++ auth0-config.js
@@ -1,6 +1,6 @@
 export const authConfig = {
     domain: import.meta.env.VITE_AUTH0_DOMAIN,
     clientId: import.meta.env.VITE_AUTH0_CLIENT,
-    redirectUri: "http://localhost:5173/dashboard",
+    redirectUri: "http://routopia-opt.netlify.app/dashboard",
   };
   
