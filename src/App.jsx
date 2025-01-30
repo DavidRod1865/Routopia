6f4272dd1a8589c1c30ee52f7bda2208cd28f8d9
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2025-01-30 18:54:19 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Routes, Route, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Landing from "./pages/Landing";
